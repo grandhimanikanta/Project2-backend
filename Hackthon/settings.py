ab7c24a4d6cf39c310fc6d2e8874694dfdfd80c6
NayakamVishnuvardhan <35533739+vishnu123sai@users.noreply.github.com>
2020-09-11 18:42:55 +0530

    Update settings.py

--- Hackthon/settings.py
+++ Hackthon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'Hackapp',
     'rest_framework',
     'rest_framework.authtoken',
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware'
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
